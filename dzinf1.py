53b69ef39fcd226deddd88b3dc740fc717e7e026
georgemaximov2006 <95126314+georgemaximov2006@users.noreply.github.com>
2023-09-14 15:26:51 +0500

    Update dzinf1.py

--- dzinf1.py
+++ dzinf1.py
@@ -2,6 +2,34 @@
 # №106
 for x in range(100, 10, -1):
     y = str(x % 4) + str(x % 2) + str(x % 5)
-    if y == '310':
+    if y == '202':
         print(x)
         break
+#Ответ: 82
+
+# №107
+k = 0
+for x in range(10, 100):
+    y = str(x % 4) + str(x % 2) + str(x % 3)
+    if y == '201':
+        k += 1
+print(k)
+#Ответ: 8
+
+#108
+k = 0
+for x in range(10, 100):
+    y = str(x % 4) + str(x % 3) + str(x % 2)
+    if y == '200':
+        k += 1
+print(k)
+#Ответ: 7
+
+#109
+k = 0
+for x in range(10, 100):
+    y = str(x % 7) + str(x % 2) + str(x % 5)
+    if y == '312':
+        k += 1
+print(k)
+#Ответ: 2
